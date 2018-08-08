31196a0c8ac94b4f6a41258cba57958e337ac599
Rhiki <42162008+Rhiki@users.noreply.github.com>
2018-08-08 13:19:20 +0900

    Update Login.java

--- Login.java
+++ Login.java
@@ -8,8 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import model.LoginLogic;
-import model.User;
+import LoginLogic;
+import User;
 
 @WebServlet("/Login")
 public class Login extends HttpServlet {
